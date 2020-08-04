e07a7f93c335cac2698baa6cba7f860f9df80497
Misha <misha.skopenko@yandex.ru>
2020-08-04 14:13:54 +0300

    add sort

--- src/Pages/PageMain/index.tsx
+++ src/Pages/PageMain/index.tsx
@@ -4,16 +4,21 @@ import Header from '../../Components/Header';
 import './style.scss';
 import { Input } from 'antd';
 import ApplicationServiсes from '../../Services'
+import { SortAscendingOutlined, SortDescendingOutlined } from '@ant-design/icons';
 
   const PageMain = () => {
 
     const { Title } = Typography;
     const [userList, setUserList ] = useState([])
     const [initUserList, setInitUserList ] = useState([])
+    const [typeSorting, setTypeSorting ] = useState({
+      desc: false,
+      asc: false,
+    })
     const [loader, setLoader] = useState(false)
     const applicationServiсes =  new ApplicationServiсes()
     useEffect(()=>{
-      applicationServiсes.getListUsers().then((list:any)=>{
+      applicationServiсes.getListUsers().then((list)=>{
         setLoader(true)
         setUserList(list)
         setInitUserList(list)
@@ -22,6 +27,41 @@ import ApplicationServiсes from '../../Services'
     },[])
 
 
+    const descSorting  = () => {
+      setTypeSorting({
+        desc:!typeSorting.desc,
+        asc:false 
+      })
+      const sortList = [...userList]
+      sortList.sort(function (a:any, b:any) {
+        if (a.id > b.id) {
+          return 1;
+        }
+        if (a.id < b.id) {
+          return -1;
+        }
+        // a должно быть равным b
+        return 0;
+      });
+      !typeSorting.desc && !typeSorting.asc?setUserList(initUserList):setUserList(sortList)
+    }
+    const  ascSorting  = async () => {
+      const sortList = [...userList]
+      setTypeSorting({
+        desc: false,
+        asc:!typeSorting.asc,
+      })
+      sortList.sort(function (a:any, b:any) {
+        if (a.id < b.id) {
+          return 1;
+        }
+        if (a.id > b.id) {
+          return -1;
+        }
+        return 0;
+      });
+    !typeSorting.desc && !typeSorting.asc?setUserList(initUserList):setUserList(sortList)
+    }
 
    const onSearch = (event:any) =>{
      const filterUserList  = [...userList]
@@ -41,12 +81,13 @@ import ApplicationServiсes from '../../Services'
       placeholder="Поиск по имени"
       onChange={value => onSearch(value)}/>
       <div className='page-main__list'>
-      {loader &&  <Spin tip="Loading...">
-     
-  </Spin>}
+        <div className='page-main__sort'>
+        <SortAscendingOutlined style={{ fontSize: '30px', color: typeSorting.asc?'red': '#08c'}} onClick={()=>ascSorting()} />
+        <SortDescendingOutlined style={{ fontSize: '30px', color: typeSorting.desc?'red': '#08c'}} onClick={()=>descSorting()}  />
+        </div>
   {userList.map((user:any)=>{
           return (
-          <div>{user.username}</div>
+          <div>{user.id} {user.username}</div>
           )
         })}
       </div>
